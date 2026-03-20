fffef0ae78d408b077e5760cdbbe4d832e58098d
aabccd021 <aabccd021@gmail.com>
2026-03-20 07:38:26 +0700

    Inline serverIp variable

--- tests.nix
+++ tests.nix
@@ -3,35 +3,31 @@
   self,
 }:
 {
-  nginx =
-    let
-      serverIp = "192.168.1.2";
-    in
-    pkgs.testers.runNixOSTest {
-      name = "subdomain-blackhole-nginx";
-      nodes.server =
-        { ... }:
-        {
-          imports = [ self.nixosModules.default ];
-          networking.interfaces.eth1.ipv4.addresses = [
-            { address = serverIp; prefixLength = 24; }
-          ];
-          services.nginx.enable = true;
-          services.nginx.virtualHosts."example.com" = {
-            onlySSL = true;
-            sslCertificate = ./test/cert.pem;
-            sslCertificateKey = ./test/key.pem;
-            locations."/".return = "200 'Hello from example.com'";
-          };
-          services.subdomain-blackhole.enable = true;
-          networking.firewall.allowedTCPPorts = [ 443 ];
+  nginx = pkgs.testers.runNixOSTest {
+    name = "subdomain-blackhole-nginx";
+    nodes.server =
+      { ... }:
+      {
+        imports = [ self.nixosModules.default ];
+        networking.interfaces.eth1.ipv4.addresses = [
+          { address = "192.168.1.2"; prefixLength = 24; }
+        ];
+        services.nginx.enable = true;
+        services.nginx.virtualHosts."example.com" = {
+          onlySSL = true;
+          sslCertificate = ./test/cert.pem;
+          sslCertificateKey = ./test/key.pem;
+          locations."/".return = "200 'Hello from example.com'";
         };
-      nodes.attacker = {
-        networking.hosts.${serverIp} = [ "example.com" "unknown.example.com" ];
-        environment.etc."ssl/server.pem".source = ./test/cert.pem;
+        services.subdomain-blackhole.enable = true;
+        networking.firewall.allowedTCPPorts = [ 443 ];
       };
-      testScript = builtins.readFile ./test/nginx.py;
+    nodes.attacker = {
+      networking.hosts."192.168.1.2" = [ "example.com" "unknown.example.com" ];
+      environment.etc."ssl/server.pem".source = ./test/cert.pem;
     };
+    testScript = builtins.readFile ./test/nginx.py;
+  };
 
   caddy = pkgs.testers.runNixOSTest {
     name = "subdomain-blackhole-caddy";
