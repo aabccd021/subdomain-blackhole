3a9737cc77f081d2cc1192eddacef9fae18663f7
aabccd021 <aabccd021@gmail.com>
2026-03-20 07:34:40 +0700

    Use shared serverIp variable instead of hardcoding IP twice

--- tests.nix
+++ tests.nix
@@ -3,27 +3,34 @@
   self,
 }:
 {
-  nginx = pkgs.testers.runNixOSTest {
-    name = "subdomain-blackhole-nginx";
-    nodes.server =
-      { ... }:
-      {
-        imports = [ self.nixosModules.default ];
-        services.nginx.enable = true;
-        services.nginx.virtualHosts."server.com" = {
-          onlySSL = true;
-          sslCertificate = ./test/cert.pem;
-          sslCertificateKey = ./test/key.pem;
-          locations."/".return = "200 'Hello from server.com'";
+  nginx =
+    let
+      serverIp = "192.168.1.2";
+    in
+    pkgs.testers.runNixOSTest {
+      name = "subdomain-blackhole-nginx";
+      nodes.server =
+        { ... }:
+        {
+          imports = [ self.nixosModules.default ];
+          networking.interfaces.eth1.ipv4.addresses = [
+            { address = serverIp; prefixLength = 24; }
+          ];
+          services.nginx.enable = true;
+          services.nginx.virtualHosts."server.com" = {
+            onlySSL = true;
+            sslCertificate = ./test/cert.pem;
+            sslCertificateKey = ./test/key.pem;
+            locations."/".return = "200 'Hello from server.com'";
+          };
+          services.subdomain-blackhole.enable = true;
+          networking.firewall.allowedTCPPorts = [ 443 ];
         };
-        services.subdomain-blackhole.enable = true;
-        networking.firewall.allowedTCPPorts = [ 443 ];
+      nodes.attacker = {
+        networking.hosts.${serverIp} = [ "server.com" "unknown.server.com" ];
       };
-    nodes.attacker = {
-      networking.hosts."192.168.1.2" = [ "server.com" "unknown.server.com" ];
+      testScript = builtins.readFile ./test/nginx.py;
     };
-    testScript = builtins.readFile ./test/nginx.py;
-  };
 
   caddy = pkgs.testers.runNixOSTest {
     name = "subdomain-blackhole-caddy";
